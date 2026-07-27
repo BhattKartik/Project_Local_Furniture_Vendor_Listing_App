34c357b9a5e322f4f5eedc21eb88603a4bc3f4b9
BhattKartik <kartikbhatt2000@gmail.com>
2026-07-27 17:31:29 +0530

    Routes for the signup added

--- back-end-part/server.js
+++ back-end-part/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const connectDB = require("./mongodbConnection_using_mongoose")
 const userModel = require("./userSchema")
 const cors = require("cors")
+const app = express();
 
 connectDB();
 
@@ -15,21 +16,27 @@ const app = express();
 
 app.get("/get",async(req,res)=>{
 
-})
-
-
-app.post("/post",async(req,res)=>{
-
-})
-
-app.delete("/delete",async(req,res)=>{
+    try{
+        let data = await userModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 
 })
 
-app.put("/put",async(req,res)=>{
-    
-})
 
+app.post("/signup",async(req,res)=>{
+
+    try{
+        let data = await userModel.create(req.body) //Here create method is used to create or save the data to the database and it will automatically call the pre save hook for hashing the password before saving it to the database
+        
+        res.status(201).json(data) //this will send the data to the client side after saving it to the database similar to res.send(data) but it will also set the status code to 201 which means created successfully
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+})
 
 
 app.listen(4000,()=>{
--- back-end-part/userSchema.js
+++ back-end-part/userSchema.js
@@ -17,24 +17,37 @@ let userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },  
+    city:{
+        type:String,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    decleration:{
+        type:Boolean,
+        required:true
     }
-
-
 
     
 })
 
 // HERE WE HAVE DONE THE PRE SAVE HOOK FOR HASHING THE PASSWORD BEFORE SAVING IT TO THE DATABASE
 
-userSchema.pre("save",async function(next){
-    if(!this.isModified("password")){
-        return next()
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
     }
 
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-})
+    this.password = await bcrypt.hash(this.password, 10); //Here 10 is the salt rounds for hashing the password which is a good practice for security and no two passwords will be same even if they are same because of the salt rounds and the hashing algorithm used by bcrypt
+    next();
+});
 
 let userModel = mongoose.model("user",userSchema,"user")
 module.exports = userModel;
